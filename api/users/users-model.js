4d59ac37f1836ec5053384bb14681ba685c60bff
Michaiah Bos <michaiahbos@yahoo.com>
2024-04-08 12:21:03 -0500

    wip

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -3,7 +3,7 @@ const db = require("../../data/db-config");
   resolves to an ARRAY with all users, each user having { user_id, username }
  */
 function find() {
-  return db("users");
+  return db("users").select("user_id", "username");
 }
 
 /**
@@ -17,7 +17,9 @@ function findBy(filter) {
   resolves to the user { user_id, username } with the given user_id
  */
 function findById(user_id) {
-  return db("users").where("user_id", user_id).first();
+  return db("users")
+  .select('user_id', 'username')
+  .where("user_id", user_id).first();
 }
 
 /**
@@ -25,7 +27,7 @@ function findById(user_id) {
  */
 async function add(user) {
   const [id] = await db("users").insert(user);
-  return findById(id)
+  return findById(id);
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
