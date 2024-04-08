2c3123ff6165fe1c81a0cbd746b65a7f31c11be4
Michaiah Bos <michaiahbos@yahoo.com>
2024-04-08 13:11:34 -0500

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,3 +1,4 @@
+const User = require("../users/users-model");
 /*
   If the user does not have a session saved in the server
 
@@ -19,8 +20,14 @@ function restricted(req, res, next) {
     "message": "Username taken"
   }
 */
-function checkUsernameFree(req, res, next) {
-  next();
+async function checkUsernameFree(req, res, next) {
+  try {
+    const user = await User.findBy({ username: req.body.username });
+    if (!user) next();
+    else next({ "message": "Username taken" });
+  } catch (err) {
+    next(err);
+  }
 }
 
 /*
